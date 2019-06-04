2b9585b321328981eaa388eebb87f4a8f9b36d4c
Demetrius King <drking445@gmail.com>
2019-06-03 17:49:13 -0700

    Changed Syntax/Naming

--- server/server.py
+++ server/server.py
@@ -14,26 +14,26 @@ async def fetch(session, url):
 
 
 class StoriesHandler(tornado.web.RequestHandler):
-    async def get(self, slug):
+    async def get(self, category):
         self.set_header("Access-Control-Allow-Origin", "*")
         loop = asyncio.get_event_loop()
         ids = loop.run_in_executor(
-            None, requests.get, 'https://hacker-news.firebaseio.com/v0/{}.json?print=pretty'.format(slug))
-        response = await ids
-        response_data = set(response.json())
-        tasks = []
+            None, requests.get, 'https://hacker-news.firebaseio.com/v0/{}.json?print=pretty'.format(category))
+        id_response = await ids
+        list_of_ids = set(id_response.json())
+        stories = []
         async with aiohttp.ClientSession() as session:
-            for url in response_data:
-                tasks.append(fetch(session, "https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty".format(
-                    url)))
-            story_list = await asyncio.gather(*tasks)
+            for id in list_of_ids:
+                stories.append(fetch(session, "https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty".format(
+                    id)))
+            story_list = await asyncio.gather(*stories)
         self.write(json.dumps(story_list, default=json_util.default))
         self.finish()
 
 
 def make_app():
     return tornado.web.Application([
-        (r"/stories/:(?P<slug>\w+)", StoriesHandler)
+        (r"/stories/:(?P<category>.*)", StoriesHandler)
     ])
 
 
